de6348784bb63df5f707bee704cb3008d2d7fed6
Daniel Bünzli <daniel.buenzli@erratique.ch>
2024-08-04 21:23:27 +0200

    Spdxify.

--- src/qrc.ml
+++ src/qrc.ml
@@ -1,6 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2020 The qrc programmers. All rights reserved.
-   Distributed under the ISC license, see terms at the end of the file.
+   SPDX-License-Identifier: ISC
   ---------------------------------------------------------------------------*)
 
 (* Any mention of section, figure or table refers to the ISO/IEC 18004:2015(E)
@@ -758,19 +758,3 @@ let encode ?mask ?version ?mode ?(ec_level = `M) s =
   match find_version ~version ~mode ~ec_level ~need:(String.length s) with
   | None -> None
   | Some (v, ec) -> Some (encode_matrix ?mask ~version:v ~ec_level:ec ~mode s)
-
-(*---------------------------------------------------------------------------
-   Copyright (c) 2020 The qrc programmers
-
-   Permission to use, copy, modify, and/or distribute this software for any
-   purpose with or without fee is hereby granted, provided that the above
-   copyright notice and this permission notice appear in all copies.
-
-   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-  ---------------------------------------------------------------------------*)
--- src/qrc_fmt.mli
+++ src/qrc_fmt.mli
@@ -1,6 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2020 The qrc programmers. All rights reserved.
-   Distributed under the ISC license, see terms at the end of the file.
+   SPDX-License-Identifier: ISC
   ---------------------------------------------------------------------------*)
 
 (** QR matrix text formatters. *)
@@ -28,19 +28,3 @@ val pp_utf_8_half : t
 (** [pp_utf_8_half] fits two vertical modules per character. It uses
     U+0020 ([' ']) and UTF-8 encoded U+2588 (FULL BLOCK), U+2584 (LOWER HALF
     BLOCK) and U+2580 (UPPER HALF BLOCK). *)
-
-(*---------------------------------------------------------------------------
-   Copyright (c) 2020 The qrc programmers
-
-   Permission to use, copy, modify, and/or distribute this software for any
-   purpose with or without fee is hereby granted, provided that the above
-   copyright notice and this permission notice appear in all copies.
-
-   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-  ---------------------------------------------------------------------------*)
