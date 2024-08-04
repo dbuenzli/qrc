3cbff87d513babcb1235ce4cece3decff63c6feb
Daniel Bünzli <daniel.buenzli@erratique.ch>
2024-08-04 23:44:24 +0200

    Fix qrtrip input and user interface. Close #5.

--- CHANGES.md
+++ CHANGES.md
@@ -1,9 +1,19 @@
 
+- Require OCaml 4.14.0.
 
+- `qrtrip` tool. Fix `stdin` reading bug, newlines were being dropped
+  and arbitrary binary input was impossible (#5).
+
+- `qrtrip` tool. Incompatible change to fix the footgun UI. Before,
+  the optional positional argument was the message to QR encode, but
+  it feels natural to specify a file to QR encode. In which case you
+  would silently QR encode a file path… The optional argument is now a
+  file to read by default. Use option `-m/--message` to treat the
+  argument as the message and make the tool operate as it used to..
+    
 - `Qrc.Matrix.to_svg`, change matrix rendering strategy. The result doesn't
   use xlink (no risk of identifier clashes when embedding), renders faster in
-  browsers and is more compact. Thanks to Alain Frisch for the patch.
-- Require OCaml 4.14.0.
+  browsers and is more compact (#1). Thanks to Alain Frisch for the patch.
 
 v0.1.0 2020-10-22 Zagreb
 ------------------------
--- test/qrtrip.ml
+++ test/qrtrip.ml
@@ -3,7 +3,19 @@
    SPDX-License-Identifier: ISC
   ---------------------------------------------------------------------------*)
 
-let strf = Format.asprintf
+let ( let* ) = Result.bind
+let exec = Filename.basename Sys.executable_name
+
+let tty_bold = "\027[01m"
+let tty_red_bold = "\027[31;01m"
+let tty_reset = "\027[m"
+let pp_code ppf s = Format.fprintf ppf "@<0>%s%s@<0>%s" tty_bold s tty_reset
+let log_err fmt =
+  Format.eprintf ("@[%s: @<0>%s%s@<0>%s: " ^^ fmt ^^ "@]@.")
+    exec tty_red_bold "Error" tty_reset
+
+let log_if_error ~use = function Ok v -> v | Error e -> log_err "%s" e; use
+
 let tty_cap_of_fd fd =
   let rec isatty fd = try Unix.isatty fd with
   | Unix.Unix_error (Unix.EINTR, _, _) -> isatty fd
@@ -14,31 +26,40 @@ let tty_cap_of_fd fd =
   | exception Not_found -> `None | "" -> `None | "dumb" -> `None
   | v -> `Ansi
 
-let stdin_to_string () =
-  let rec loop acc = match input_line stdin with
-  | l -> loop (l :: acc)
-  | exception End_of_file -> Ok (String.concat "" (List.rev acc))
-  | exception Sys_error e -> Error e
+let read_file file =
+  let read file ic = try Ok (In_channel.input_all ic) with
+  | Sys_error e -> Error (Printf.sprintf "%s: %s" file e)
   in
-  loop []
+  let binary_stdin () = In_channel.set_binary_mode In_channel.stdin true in
+  try match file with
+  | "-" -> binary_stdin (); read file In_channel.stdin
+  | file -> In_channel.with_open_bin file (read file)
+  with Sys_error e -> Error e
 
-let string_to_file file s =
-  try
-    let oc = open_out_bin file in
-    let finally () = close_out_noerr oc in
-    Ok (Fun.protect ~finally @@ fun () -> output_string oc s)
+let write_file file s =
+  let write file s oc = try Ok (Out_channel.output_string oc s) with
+  | Sys_error e -> Error (Printf.sprintf "%s: %s" file e)
+  in
+  let binary_stdout () = Out_channel.(set_binary_mode stdout true) in
+  try match file with
+  | "-" -> binary_stdout (); write file s Out_channel.stdout
+  | file -> Out_channel.with_open_bin file (write file s)
   with Sys_error e -> Error e
 
 (* Encoding *)
 
+let err_capacity = 1
+
+let format_for_stdout () = match tty_cap_of_fd Unix.stdout with
+| `None -> `Unicode_full | `Ansi -> `Ansi
+
 let get_format outf = function
-| Some f -> f
+| Some format -> format
 | None ->
     match outf with
-    | Some f when not (String.equal "-" f) -> `Unicode_full
-    | _ ->
-        match tty_cap_of_fd Unix.stdout with
-        | `None -> `Unicode_full | `Ansi -> `Ansi
+    | "-" -> format_for_stdout ()
+    | file when String.ends_with ~suffix:".svg" file -> `Svg
+    | _ -> `Unicode_full
 
 let output_matrix format outf ~invert ~quiet_zone m =
   let pp_svg ?invert ?quiet_zone ppf m =
@@ -51,30 +72,30 @@ let output_matrix format outf ~invert ~quiet_zone m =
   | `Unicode_half -> Qrc_fmt.pp_utf_8_half
   | `Unicode_full -> Qrc_fmt.pp_utf_8_full
   in
-  let qr = strf "@[%a@]" (pp ~invert ~quiet_zone) m in
-  match outf with
-  | None -> print_string qr; Ok ()
-  | Some outf -> string_to_file outf qr
+  let qr = Format.asprintf "@[%a@]" (pp ~invert ~quiet_zone) m in
+  write_file outf qr
 
-let encode format outf invert no_quiet_zone version ec_level mask data =
-  let get_data = function None -> stdin_to_string () | Some d -> Ok d in
-  Result.bind (get_data data) @@ fun data ->
+let encode
+    ~format ~outf ~invert ~no_quiet_zone ~version ~ec_level ~mask ~is_msg
+    ~file_or_msg
+  =
+  let* data = if is_msg then Ok file_or_msg else read_file file_or_msg in
   let quiet_zone = not no_quiet_zone in
   let version = Option.map (fun v -> `V v) version in
   match Qrc.encode ?mask ?version ?ec_level data with
   | Some m ->
-      Result.map Result.ok (output_matrix format outf ~invert ~quiet_zone m)
+      let* () = output_matrix format outf ~invert ~quiet_zone m in
+      Ok Cmdliner.Cmd.Exit.ok
   | None ->
       let ec_level = match ec_level with None -> `M | Some l -> l in
       let version = match version with None -> `V 40 | Some v -> v in
-      let err = strf
-          "QR code capacity of %d bytes exceeded: %d data bytes provided\n\
-           Try to increase the version (-v) or reduce the error correction \
-           level (-c)"
-          (Qrc.Prop.mode_capacity version ec_level `Byte)
-          (String.length data)
-      in
-      Ok (Error err)
+      log_err
+        "@[<v>QR capacity of %a bytes exhausted: data has %a bytes@,\
+         Reduce error correction with %a or icrease the version with %a"
+        pp_code (Int.to_string (Qrc.Prop.mode_capacity version ec_level `Byte))
+        pp_code (Int.to_string (String.length data))
+        pp_code "-c" pp_code "-v";
+      Ok err_capacity
 
 (* Decoding *)
 
@@ -82,16 +103,16 @@ let decode () = Error "Sorry, unimplemented yet."
 
 (* Tripping *)
 
-let qrtrip enc format outf invert no_quiet_zone version ec_level mask data  =
-  let log_err e = Format.eprintf "@[%s: @[%s@]@]@." Sys.executable_name e in
-  let r = match enc with
+let qrtrip
+    ~encode:enc ~format ~outf ~invert ~no_quiet_zone ~version ~ec_level ~mask
+    ~is_msg ~file_or_msg
+  =
+  log_if_error ~use:Cmdliner.Cmd.Exit.some_error @@
+  match enc with
   | `Decode -> decode ()
   | `Encode ->
-      encode format outf invert no_quiet_zone version ec_level mask data
-  in
-  match r with
-  | Error e -> log_err e; 2
-  | Ok r -> match r with Ok () -> 0 | Error e -> log_err e; 1
+      encode ~format ~outf ~invert ~no_quiet_zone ~version ~ec_level ~mask
+        ~is_msg ~file_or_msg
 
 (* Command line interface *)
 
@@ -118,11 +139,14 @@ let format =
      $(b,ansi) outputs ANSI terminal escape sequences, \
      $(b,svg) outputs an SVG image,
      $(b,text) outputs UTF-8 encoded Unicode full blocks, \
-     $(b,half) outputs UTF-8 encoded Unicode half blocks."
+     $(b,half) outputs UTF-8 encoded Unicode half blocks. If absent \
+     $(b,svg) is used if the file of $(b,-o) ends with $(b,.svg), $(b,ansi)
+     or $(b,full) is used on $(b,stdout) and $(b,full) otherwise.";
   in
   let env = Cmd.Env.info "QRTRIP_FORMAT" and docv = "FORMAT" in
-  let fconv = Arg.(some ~none:"ansi or text" (Arg.enum formats)) in
-  Arg.(value & opt fconv None & info ~doc ~docv ["f"; "format"] ~env)
+  let absent = "guess" in
+  let fconv = Arg.some (Arg.enum formats) in
+  Arg.(value & opt fconv None & info ~absent ~doc ~docv ["f"; "format"] ~env)
 
 let invert =
   let doc = "Invert QR matrix, black modules are white and vice-versa." in
@@ -135,29 +159,35 @@ let no_quiet_zone =
   Arg.(value & flag & info ["no-quiet"] ~doc)
 
 let outf =
-  let doc = "Output to $(docv)." and docv = "FILE" in
-  Arg.(value & opt (some ~none:"stdout" string) None & info ["o"] ~doc ~docv)
+  let doc = "Output to $(docv). Standard output if unspecified." in
+  let docv = "FILE" in
+  Arg.(value & opt string "-" & info ["o"] ~doc ~docv)
 
 let version =
   let doc =
     "Use QR code $(docv) (1-40) for encoding. This defines the matrix width \
      (21-177). Not recommended, let the minimal needed version be selected."
   in
-  let vconv = int_range ~lo:1 ~hi:40 "version" and none = "minimal needed" in
-  Arg.(value & opt (some ~none vconv) None &
-       info ~doc ~docv:"VERSION" ["v"; "qr-version"])
+  let vconv = int_range ~lo:1 ~hi:40 "version" and absent = "minimal needed" in
+  Arg.(value & opt (some vconv) None &
+       info ["v"; "qr-version"] ~absent ~doc ~docv:"VERSION")
 
 let mask =
   let doc =
     "Force QR mask to $(docv) (0-7) for encoding. This option should \
      not be used, it is used for debugging."
   in
-  let mconv = int_range ~lo:0 ~hi:7 "mask" and none = "standard compliant" in
-  Arg.(value & opt (some ~none mconv) None & info ~doc ~docv:"MASK" ["mask"])
+  let mconv = int_range ~lo:0 ~hi:7 "mask" and absent = "standard compliant" in
+  Arg.(value & opt (some mconv) None & info ["mask"] ~absent ~doc ~docv:"MASK")
 
-let data =
-  let doc = "Data to encode." and none = "read from stdin" in
-  Arg.(value & pos 0 (some ~none string) None & info ~doc ~docv:"DATA" [])
+let is_msg =
+  let doc = "Positional argument is the message to encode, not a file path." in
+  Arg.(value & flag & info ["m"; "message"] ~doc)
+
+let file_or_msg =
+  let doc = "File or message (with option $(b,-m)) to encode. \
+             Standard input if unspecified." in
+  Arg.(value & pos 0 string "-" & info [] ~doc ~docv:"FILE")
 
 let ec_level =
   let levels = [ "L", `L; "M", `M; "Q", `Q; "H", `H ] in
@@ -169,30 +199,40 @@ let ec_level =
       (Arg.doc_alts_enum levels)
   in
   let lconv = Arg.some ~none:"M" (Arg.enum levels) in
-  Arg.(value & opt lconv None & info ~doc ~docv:"LEVEL" ["c"; "ec-level"])
+  Arg.(value & opt lconv None & info ["c"; "ec-level"] ~doc ~docv:"LEVEL")
 
 let encode =
-  let enc = Arg.info ~doc:"Encode a QR code (default)." ["e"; "encode"] in
-  let dec = Arg.info ~doc:"Decode a QR code (unimplemented)." ["d"; "decode"] in
+  let enc = Arg.info ["e"; "encode"] ~doc:"Encode a QR code (default)." in
+  let dec = Arg.info ["d"; "decode"] ~doc:"Decode a QR code (unimplemented)." in
   Arg.(value & vflag `Encode  [`Encode, enc; `Decode, dec])
+
+let err_capacity = 1
 
 let qrtrip =
   let doc = "QR encode data" in
   let man = [
     `S Manpage.s_description;
-    `P "$(tname) encodes data into QR codes.";
+    `P "$(iname) encodes data into QR codes. Examples:";
+    `Pre "$(iname) $(b,/path/to/file)            # QR code for file contents";
+    `Noblank;
+    `Pre "$(iname) $(b,-m https://example.org)   \
+          # QR code for https://example.org";
+    `Noblank;
+    `Pre "$(iname) $(b,-m https://example.org -o example.svg)"; `Noblank;
+    `Pre "$(iname) $(b,-f svg -m https://example.org > example.svg)";
     `S Manpage.s_bugs;
     `P "This program is distributed with the Qrc OCaml library.
         See https://erratique.ch/software/qrc for contact information."; ]
   in
   let exits =
-    Cmd.Exit.info ~doc:"on QR code capacity exceeded." 1 ::
-    Cmd.Exit.info ~doc:"on indiscriminate error reported on stderr." 2 ::
+    Cmd.Exit.info ~doc:"on QR code capacity exceeded." err_capacity ::
     Cmd.Exit.defaults
   in
   Cmd.v (Cmd.info "qrtrip" ~version:"%%VERSION%%" ~doc ~man ~exits) @@
-  Term.(const qrtrip $ encode $ format $ outf $ invert $ no_quiet_zone $
-        version $ ec_level $ mask $ data)
+  let+ encode and+ format and+ outf and+ invert and+ no_quiet_zone
+  and+ version and+ ec_level and+ mask and+ is_msg and+ file_or_msg in
+  qrtrip ~encode ~format ~outf ~invert ~no_quiet_zone
+    ~version ~ec_level ~mask ~is_msg ~file_or_msg
 
 let main () = Cmd.eval' qrtrip
 let () = if !Sys.interactive then () else exit (main ())
